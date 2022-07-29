b3397c6fe9cfbf600baf62417f6a8a9d0fbb6912
Strive <1275610725>
2022-07-29 15:30:34 +0800

    基础单例

--- src/main/java/DesignMode/Singleton/HungrySingleton.java
+++ src/main/java/DesignMode/Singleton/HungrySingleton.java
@@ -2,13 +2,15 @@ package DesignMode.Singleton;
 
 /**
  * 饿汉式
+ * 类加载到内存后，就实例化一个单例，JVM保证线程安全
+ * 简单实用
+ * 唯一的缺点：不管是否用到，类加载时就完成实例化（不管是否用到，只要类一加载类就会初始化）
  */
 public class HungrySingleton {
 
     private static final HungrySingleton hungrySingleton = new HungrySingleton();
 
-    private HungrySingleton() {
-    }
+    private HungrySingleton() {}
 
     public static HungrySingleton getInstance() {
         return hungrySingleton;
