2947cbaf51e3e45435d247ccb8b71552c34efa2f
Strive <1275610725>
2022-09-13 15:29:33 +0800

    有一个颗树，先序遍历的结果是pre[L1...R1],中序结果是in[L2...R2]

--- src/main/java/algorithm/myTree/MyTree01.java
+++ src/main/java/algorithm/myTree/MyTree01.java
@@ -19,34 +19,42 @@ public class MyTree01 {
         return h1.val == h2.val && isMirror(h1.left, h2.right) && isMirror(h1.right, h2.left);
     }
 
+    /**
+     * 返回最大树深
+     */
+    private static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
     public static void main(String[] args) {
-        TreeNode root = new TreeNode();
-        root.val = 9;
-        TreeNode left = new TreeNode();
-        left.val = 7;
-        TreeNode left1 = new TreeNode();
-        left1.val = 4;
-        TreeNode right1 = new TreeNode();
-        right1.val = 2;
-        left.left = left1;
-        left.right = right1;
-
-        TreeNode right = new TreeNode();
-        right.val = 7;
-        TreeNode left2 = new TreeNode();
-        left2.val = 2;
-        TreeNode right2 = new TreeNode();
-        right2.val = 4;
-        right.left = left2;
-        right.right = right2;
-        root.left = left;
-        root.right = right;
+        TreeNode root = getTestTree();
         System.out.println(isSymmetric(root));
-
+        System.out.println(maxDepth(root));
 
         //二进制中0代表false，1代表true
         System.out.println(true ^ true);
         System.out.println(true ^ false);
         System.out.println(false ^ false);
+    }
+
+    public static TreeNode getTestTree(){
+        TreeNode root = new TreeNode(9);
+        TreeNode left = new TreeNode(7);
+        TreeNode left1 = new TreeNode(4);
+        TreeNode right1 = new TreeNode(2);
+        left.left = left1;
+        left.right = right1;
+
+        TreeNode right = new TreeNode(7);
+        TreeNode left2 = new TreeNode(2);
+        TreeNode right2 = new TreeNode(4);
+        right.left = left2;
+        right.right = right2;
+        root.left = left;
+        root.right = right;
+        return root;
     }
 }
