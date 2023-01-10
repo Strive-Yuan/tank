37a4d81ba8bb8928ab7bf49278564562126417a7
Strive <1275610725>
2023-01-10 10:18:23 +0800

    测试接口

--- src/main/java/com/redis/UserController.java
+++ src/main/java/com/redis/UserController.java
@@ -1,7 +1,14 @@
 package com.redis;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
 
 
-
+    @GetMapping("findAll")
+    public String findAll(){
+        return  "yjhui是真滴帅!";
+    }
 }
