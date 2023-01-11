0cad9c6aca23dca4de62aefc4cce0e9137f96d26
Strive <1275610725>
2023-01-11 09:26:10 +0800

    redis

--- src/main/java/com/MyAlgorithmApplication.java
+++ src/main/java/com/MyAlgorithmApplication.java
@@ -1,11 +1,15 @@
 package com;
 
+import com.redis.RedisDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MyAlgorithmApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MyAlgorithmApplication.class, args);
+        ConfigurableApplicationContext cxt = SpringApplication.run(MyAlgorithmApplication.class, args);
+        RedisDemo redisDemo = cxt.getBean(RedisDemo.class);
+        redisDemo.setString();
     }
 }
---
+++ src/main/java/com/configuration/RedisConfiguration.java
@@ -0,0 +1,17 @@
+package com.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
+
+@Configuration
+public class RedisConfiguration {
+
+
+//    public Object redisConnectFactory(){
+//        RedisConfiguration redisConfiguration = new RedisConfiguration();
+//
+//        Object o = redisConfiguration.redisConnectFactory();
+//
+//    }
+}
