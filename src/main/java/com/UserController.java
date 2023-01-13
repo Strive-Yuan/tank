4b03058ca267eb1c1210c55d69e8c1e24e6d44d1
Strive <1275610725>
2023-01-13 14:38:56 +0800

    kafka

--- src/main/java/com/UserController.java
+++ src/main/java/com/UserController.java
@@ -1,14 +1,11 @@
 package com;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.redis.Person;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.hash.Jackson2HashMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.Map;
 
 @RestController
 public class UserController {
