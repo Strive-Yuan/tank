3fc2928200ef211be79b7e13b1bcce8cb500c5d2
Strive <1275610725>
2023-01-20 22:45:33 +0800

    1.全局异常处理重构 2.自定义异常重构

--- src/main/java/com/HubApplication.java
+++ src/main/java/com/HubApplication.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.api.*")
-public class MyAlgorithmApplication {
+public class HubApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MyAlgorithmApplication.class, args);
+        SpringApplication.run(HubApplication.class, args);
     }
 }
