bae2acca180b9469b4540c09fb8437c76133b0e5
Strive <1275610725>
2023-02-03 11:42:20 +0800

    登录接口更新

--- src/main/java/com/api/server/impl/UserServiceImpl.java
+++ src/main/java/com/api/server/impl/UserServiceImpl.java
@@ -3,13 +3,10 @@ package com.api.server.impl;
 import com.api.mapper.UserMapper;
 import com.api.pojo.User;
 import com.api.server.UserService;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.List;
-import java.util.concurrent.LinkedBlockingQueue;
 
 @Service
 public class UserServiceImpl implements UserService {
