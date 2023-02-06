5f4eaaaefcab03d3a93a0a97acef362ebc453b8a
Strive <1275610725@qq.com>
2023-02-06 23:09:43 +0800

    是否是平衡树

---
+++ src/main/java/com/module/algorithm/myTree/MyTree05.java
@@ -0,0 +1,12 @@
+package com.module.algorithm.myTree;
+
+/**
+ * 判断是否是一个二叉搜索树
+ */
+public class MyTree05 {
+
+
+    /**
+     * 方案1:利用中序遍历（二叉树的中序遍历结果一定是递增的）
+     */
+}
---
+++ src/main/java/com/module/algorithm/myTree/MyTree06.java
@@ -0,0 +1,8 @@
+package com.module.algorithm.myTree;
+
+/**
+ * 判断是否是平衡搜索二叉树
+ * https://leetcode.com/problems/balanced-binary-tree/
+ */
+public class MyTree06 {
+}
