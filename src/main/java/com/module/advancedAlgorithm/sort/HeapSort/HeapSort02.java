7a7c22ae041a2b395a79e3f80f659f4e74f31cc5
Strive <1275610725@qq.com>
2023-03-09 23:11:28 +0800

    最大线段重合问题(未实现)

---
+++ src/main/java/com/module/advancedAlgorithm/sort/HeapSort/HeapSort02.java
@@ -0,0 +1,8 @@
+package com.module.advancedAlgorithm.sort.HeapSort;
+
+/**
+ *  已知一个几乎有序的数组。几乎有序是指，如果吧数组排好顺序的话，每个元素移动的距离一定不超过k，并且k相对于数组长度来说是比较小的
+ *  请选择一个合适的排序策略，对这个数组进行排序
+ */
+public class HeapSort02 {
+}
