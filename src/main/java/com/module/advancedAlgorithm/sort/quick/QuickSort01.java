b27019a5037f74781b08df543e48a778a9f6b65d
Strive <1275610725>
2023-03-09 10:06:20 +0800

    1.快速排序

--- src/main/java/com/module/advancedAlgorithm/sort/quick/QuickSort01.java
+++ src/main/java/com/module/advancedAlgorithm/sort/quick/QuickSort01.java
@@ -1,5 +1,9 @@
 package com.module.advancedAlgorithm.sort.quick;
 
+import com.module.advancedAlgorithm.sort.ArrayUtils;
+
+import java.util.Arrays;
+
 /**
  * 快速排序
  * 荷兰国旗问题
@@ -7,14 +11,40 @@ package com.module.advancedAlgorithm.sort.quick;
  * [<arr[R], ==arr[R], >arr[R]]
  */
 public class QuickSort01 {
-    public static void QuickSort(int[] arr) {
+    // for test
+    public static void main(String[] args) {
+        int[] arr = {1, 82, 35, 46, 5, 6, 19, -50, 30};
+        quickSort(arr);
+        Arrays.stream(arr).forEach(num -> System.out.print(" " + num));
+    }
+
+    public static void quickSort(int[] arr) {
         if (arr == null || arr.length < 2) {
             return;
         }
-        process(arr, arr.length - 1);
+        partition(arr, arr[arr.length - 1]);
     }
 
-    private static void process(int[] arr, int i) {
+    private static void partition(int[] arr, int targetValue) {
+        int leftGroupIndex = -1;
+        int rightGroupIndex = arr.length - 1;
+        int curValueIndex = 0;
+        while (curValueIndex < rightGroupIndex) {
+            if (arr[curValueIndex] < targetValue) {
+                curValueIndex++;
+                leftGroupIndex++;
+            } else if (arr[curValueIndex] == targetValue) {
+                curValueIndex++;
+            } else if (arr[curValueIndex] > targetValue) {
+                swap(arr, curValueIndex, --rightGroupIndex);
+            }
+        }
+        swap(arr, curValueIndex, arr.length - 1);
+    }
 
+    private static void swap(int[] arr, int num1, int num2) {
+        int temp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = temp;
     }
 }
